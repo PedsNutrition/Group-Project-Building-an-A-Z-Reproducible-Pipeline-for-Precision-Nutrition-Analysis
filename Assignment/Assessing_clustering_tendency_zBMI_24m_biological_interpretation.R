f55322af8c49d83b071ba169ae1cc852dfaa0c3f
evakranenburg <eva.kranenburg@mail.utoronto.ca>
2026-04-06 23:35:20 -0400

    Update Assessing_clustering_tendency_zBMI_24m_biological_interpretation.R

--- Assignment/Assessing_clustering_tendency_zBMI_24m_biological_interpretation.R
+++ Assignment/Assessing_clustering_tendency_zBMI_24m_biological_interpretation.R
@@ -4,7 +4,7 @@
 # Version:  2
 #
 # Date:  2026  Apr
-# Authors: Steven Chen, Brinley Klievik, Eva Kranenburg
+# Authors: Yue-Tong Chen, Brinley Klievik, Triyani Komang, Eva Kranenburg
 #
 # Original Author:   Paraskevi Massara (p.massara@utoronto.ca)
 #
